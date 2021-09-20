c33a44657902b5fa7748bb5fa7df16e115a2e390
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-20 11:56:51 -0700

    xc

--- src/main/java/Vigenere.java
+++ src/main/java/Vigenere.java
@@ -1,14 +1,77 @@
 import java.util.Scanner;
 
+
 public class Vigenere {
     public static String encryptVigenere(String message, String key) {
-        return message;
+        String new_msg = "";
+        int count = 0;
+        for(int i=0;i<message.length();i++){
+            if ((int) message.charAt(i) < 65 || ((int) message.charAt(i) > 90 && (int) message.charAt(i) < 97) || (int) message.charAt(i) > 122){
+                new_msg += message.charAt(i);
+            }
+            else{
+                new_msg += encryptCaesarLetter(message.charAt(i), Math.abs((key.charAt(count % key.length()) - 65)));
+                count ++;
+            }
+        }
+        return new_msg;
         // REPLACE THIS WITH YOUR CODE
+
     }
 
     public static String decryptVigenere(String message, String key) {
-        return message;
+        String new_msg = "";
+        int count = 0;
+        for(int i=0;i<message.length();i++){
+            if ((int) message.charAt(i) < 65 || ((int) message.charAt(i) > 90 && (int) message.charAt(i) < 97) || (int) message.charAt(i) > 122){
+                new_msg += message.charAt(i);
+            }
+            else{
+                new_msg += decryptCaesarLetter(message.charAt(i), Math.abs((key.charAt(count % key.length()) - 65)));
+                count ++;
+            }
+        }
+        return new_msg;
         // REPLACE THIS WITH YOUR CODE
+    }
+    public static char encryptCaesarLetter(char ch, int key) {
+        // YOUR CODE HERE
+        int chAscii = (int) ch;
+        if (chAscii >= 65 && chAscii <= 90){
+            if(chAscii >= 90 - key + 1){
+                return (char) (chAscii - (26 - key));
+            }
+            return (char) (chAscii + key);
+        }
+        else if (chAscii >= 97 && chAscii <= 122){
+            if(chAscii >= 122 - key + 1){
+                return (char) (chAscii - (26 - key));
+            }
+            return (char) (chAscii + key);
+        }
+        else{
+            return ch;
+        }
+    }
+
+    public static char decryptCaesarLetter(char ch, int key) {
+        // YOUR CODE HERE
+        int chAscii = (int) ch;
+        if (chAscii >= 65 && chAscii <= 90){
+            if(chAscii <= 65 + key - 1){
+                return (char) (chAscii + (26 - key));
+            }
+            return (char) (chAscii - key);
+        }
+        else if (chAscii >= 97 && chAscii <= 122){
+            if(chAscii <= 97 + key - 1){
+                return (char) (chAscii + (26 - key));
+            }
+            return (char) (chAscii - key);
+        }
+        else{
+            return ch;
+        }
     }
 
 
