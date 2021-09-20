5c7ad3325e11aba7d1bfe811547b221a0eeb9e3d
anaypant <35899820+anaypant@users.noreply.github.com>
2021-09-20 11:34:36 -0700

    xc

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -73,15 +73,67 @@ public class Caesar {
     // Ex. encryptCaesarKey("Hello, World!", 5) => "Mjqqt, Btwqi!".
     public static String encryptCaesarKey(String message, int key) {
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        String new_msg = "";
+        for (int i=0;i<message.length();i++){
+            int currentAscii = (int) message.charAt(i);
+            if (currentAscii >= 97 && currentAscii <= 122){
+                if (currentAscii >= 122 - key + 1){
+                    new_msg = new_msg + (char) (currentAscii - (26 - key));
+
+                }
+                else {
+                    new_msg = new_msg + (char) (currentAscii + key);
+                }
+            }
+
+            else if (currentAscii >= 65 && currentAscii <= 90){
+                if (currentAscii >= 90 - key + 1){
+                    new_msg = new_msg + (char) (currentAscii - (26 - key));
+
+                }
+                else {
+                    new_msg = new_msg + (char) (currentAscii + key);
+                }
+            }
+            else{
+                new_msg = new_msg + (char) (currentAscii);
+            }
+        }
+        return new_msg;
     }
+
 
     // Given a String and a key corresponding to a shift, decrypts
     // the String using the given key and returns the original String
     // Ex. decryptCaesarKey("Mjqqt, Btwqi!", 5) => "Hello, World!"
     public static String decryptCaesarKey(String message, int key) {
         // REPLACE THIS WITH YOUR CODE
-        return message;
+        String new_msg = "";
+        for (int i=0; i<message.length();i++){
+            int currentAscii = (int) message.charAt(i);
+            if (currentAscii >= 65 && currentAscii <= 90){
+                if (currentAscii <= 65 + key - 1){
+                    new_msg += (char) (currentAscii + (26 - key));
+                }
+                else{
+                    new_msg += (char) (currentAscii - key);
+                }
+
+            }
+            else if (currentAscii >= 97 && currentAscii <= 122){
+                if (currentAscii <= 97 + key - 1){
+                    new_msg += (char) (currentAscii + (26 - key));
+                }
+                else{
+                    new_msg += (char) (currentAscii - key);
+                }
+            }
+            else{
+                new_msg += (char) currentAscii;
+            }
+
+        }
+        return  new_msg;
     }
 
 
@@ -96,14 +148,18 @@ public class Caesar {
         if (command.equals("encrypt")) {
             System.out.println("Please enter your message to be encrypted: ");
             String message = scan.nextLine();
+            System.out.println("Please enter the key for your message:");
+            int key = Integer.parseInt(scan.nextLine());
             System.out.println("Here is your encrypted message: ");
-            System.out.println(encryptCaesar(message));
+            System.out.println(encryptCaesarKey(message, key));
         }
         else if (command.equals("decrypt")) {
             System.out.println("Please enter your message to be decrypted: ");
             String message = scan.nextLine();
+            System.out.println("Please enter the key for your message:");
+            int key = Integer.parseInt(scan.nextLine());
             System.out.println("Here is your decrypted message: ");
-            System.out.println(decryptCaesar(message));
+            System.out.println(decryptCaesarKey(message, key));
         }
         else {
             System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
